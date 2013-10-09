30e1dd8111be699bb5758e123917a3d5409c84d9
mbirostris <olszewski.mikael@gmail.com>
2013-10-09 19:17:44 +0200

    dalej

--- eta_w_beta.py
+++ eta_w_beta.py
@@ -13,6 +13,13 @@ from mpl_toolkits.axes_grid1 import host_subplot
 import mpl_toolkits.axisartist as AA
 
 
+towers = [0.07, 0.27, 0.44, 0.58, 0.72,0.83,0.93,1.04,1.14,1.24,1.36,1.48,1.61]
+def toTower(x):
+    for i in xrange(0,13):
+        if (math.fabs(x) > towers[i]): continue;
+        return i;
+
+
 try:
     os.mkdir("./rysunki/tree/")
 except:
@@ -34,25 +41,26 @@ plik=str(sys.argv[1]);
 rec = root2rec(plik+'.root', "tvec")
 
 
-towers = [0.07, 0.27, 0.44, 0.58, 0.72,0.83,0.93,1.04,1.14,1.24,1.36,1.48,1.61]
+beta = [0.4, 0.5, 0.6, 0.7, 0.8,0.9,1.]
 colors = ['b', 'g','r','c','m','y','k','w']
 
-x=[[],[],[],[],[],[],[],[],[],[],[],[],[]];
-z=[[],[],[],[],[],[],[],[],[],[],[],[],[]];
+x=[[],[],[],[],[],[],[]];
+z=[[],[],[],[],[],[],[]];
 for i in xrange(0, len(rec)):
-    for j in xrange(0,13):
-        if (math.fabs(rec.Teta[i]) > towers[j]):
+    for j in xrange(0,7):
+        if (rec.Tbeta[i] > beta[j]):
             continue;
-        x[j].append(rec.Tbeta[i]);
+        x[j].append(toTower(rec.Teta[i]));
         if (rec.Tl1[i]):
-            z[j].append(rec.Tbeta[i])
+            z[j].append(toTower(rec.Teta[i]))
         break;    
-for i in xrange(0,8):
-    histMC, bins =  np.histogram(x[i], bins=np.arange(0.,1.,0.01));
-    histL1, bins = np.histogram(z[i], bins=np.arange(0.,1.,0.01));
+
+for i in xrange(0,7):
+    histMC, bins =  np.histogram(x[i], bins=np.arange(0.,13.,1));
+    histL1, bins = np.histogram(z[i], bins=np.arange(0.,13.,1));
     po = np.divide(np.array(histL1, dtype=float), np.array(histMC, dtype=float))
     #    plt.figure(i);
-    plt.plot(np.arange(0.,0.99,0.01), po, '-', color=colors[i])
+    plt.plot(np.arange(0.,12.,1), po, '-', color=colors[i])
 
 
 plt.xlabel('Smarts')
